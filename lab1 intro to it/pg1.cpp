a845c4d95f2df32e25174a488539ca6af84509ab
Monanbek <metamaskakk1@gmail.com>
2026-05-19 13:53:37 +0300

    Add files via upload

---
+++ lab1 intro to it/pg1.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Student
+{
+    string fio;
+    char pol;
+    double stipendiya;
+};
+
+int main()
+{
+    int n;
+    cout << "Kolichestvo studentov: ";
+    cin >> n;
+
+    Student* students = new Student[n];
+
+    cout << "Vvedite dannye studentov:" << endl;
+
+    for (int i = 0; i < n; ++i)
+    {
+        cin.ignore();
+
+        cout << "FIO: ";
+        getline(cin, students[i].fio);
+
+        cout << "Pol (M/F): ";
+        cin >> students[i].pol;
+
+        cout << "Stipendiya: ";
+        cin >> students[i].stipendiya;
+    }
+
+    char nuzhniyPol;
+    cout << "Vvedite nuzhniy pol: ";
+    cin >> nuzhniyPol;
+
+    cout << "Result:" << endl;
+
+    for (int i = 0; i < n; ++i)
+    {
+        if (students[i].pol == nuzhniyPol && students[i].stipendiya == 0)
+            cout << students[i].fio << endl;
+    }
+
+    delete[] students;
+    return 0;
+}
